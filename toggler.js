4afa50edb80442e8801556cf983cd7884418acbe
Neuber Oliveira <neuber.oliveira05@gmail.com>
2021-04-30 11:04:24 -0300

    chore: support to work on localhost

--- toggler.js
+++ toggler.js
@@ -1,6 +1,12 @@
-const toggle = (parent)=>{
-  parent.querySelectorAll('.database-node.type-array > .database-node-click-target, .database-node.type-map > .database-node-click-target').forEach((el)=>el.click())
+const toggle = (parent, query)=>{
+  parent.querySelectorAll(query).forEach((el)=>el.click())
 }
+
+const toggleInConsole = (parent)=>toggle(parent, '.database-node.type-array > .database-node-click-target, .database-node.type-map > .database-node-click-target')
+
+const toggleInLocalhost = (parent)=>toggle(parent, '.List-Actions .FieldPreview:not(.FieldPreview--primitive)')
+const isLocalhost = window.location.host.indexOf('localhost')!==-1 || window.location.host.indexOf('127.0.0.1')!==-1
+
 
 var observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
@@ -9,8 +15,12 @@ var observer = new MutationObserver(function (mutations) {
     }
     for (var i = 0; i < mutation.addedNodes.length; i++) {
       node = mutation.addedNodes[i]
-      if(node.tagName==='FS-ANIMATE-CHANGES'){
-        toggle(node)
+      console.log(node)
+      if(isLocalhost && node.querySelector('.List-Actions .FieldPreview:not(.FieldPreview--primitive)')){
+        toggleInLocalhost(node)
+
+      }else if(node.tagName==='FS-ANIMATE-CHANGES'){
+        toggleInConsole(node)
       }
     }
   });
